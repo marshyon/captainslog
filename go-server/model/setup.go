8515ac644fd1ce17801815b87e7010f44cec00f1
Jon Brookes <marshyon@gmail.com>
2021-05-03 14:15:02 +0100

    initial commit

--- go-server/model/setup.go
+++ go-server/model/setup.go
@@ -1,6 +1,9 @@
 package model
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
 )
@@ -8,6 +11,16 @@ import (
 var DB *gorm.DB
 
 func ConnectDatabase() {
+
+	dbUsername := os.Getenv("DB_USERNAME")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbHost := os.Getenv("DB_HOST")
+	dbTable := os.Getenv("DB_TABLE")
+	dbPort := os.Getenv("DB_PORT")
+
+	// dbConnectionString := dbUsername + ":" + dbPassword + "@tcp(" + dbHost + ":" + strconv.Itoa(dbPort) + ")/" + dbTable
+	connectionString := fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=disable", dbHost, dbPort, dbUsername, dbTable, dbPassword)
+	fmt.Println(connectionString)
 	database, err := gorm.Open("sqlite3", "test.db")
 
 	if err != nil {
